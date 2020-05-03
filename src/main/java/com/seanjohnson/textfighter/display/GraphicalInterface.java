0cfa2c96d23fe539db6d6cba618509edcab2c369
pigcoder3 <pigcoder3@gmail.com>
2020-05-03 09:47:58 -0400

    wrote tests for autocompletion

--- src/main/java/com/seanjohnson/textfighter/display/GraphicalInterface.java
+++ src/main/java/com/seanjohnson/textfighter/display/GraphicalInterface.java
@@ -1,6 +1,7 @@
 package com.seanjohnson.textfighter.display;
 
 import com.seanjohnson.textfighter.HistoryLinkedList;
+import com.seanjohnson.textfighter.Player;
 import com.seanjohnson.textfighter.TextFighter;
 import com.seanjohnson.textfighter.location.Choice;
 
@@ -160,53 +161,15 @@ public class GraphicalInterface extends JFrame {
 						break;
 					case(KeyEvent.VK_TAB): //Autocomplete commands with tab (as is done in most terminals)
 						if(inputArea.getText().length() == 0 || TextFighter.player.getLocation().getPossibleChoices().size() == 0) { break; } // Dont do anything if there is no input or there are no possible choices
-						//This gets the first command (getting the rest of the commands is too resources intensive as of all the ways I've tried).
-						String currentInputText = inputArea.getText();
-						String[] possibleWantedCommands = new String[TextFighter.player.getLocation().getPossibleChoices().size()];
-						int i = 0;
-						for(Choice c : TextFighter.player.getLocation().getPossibleChoices()) {
-							if(c.getName().startsWith(currentInputText)) {
-								possibleWantedCommands[i] = c.getName();
-								i++; //increase in possibleWantedCommands
-							}
-						}
-						if(possibleWantedCommands[0] == null) { break; } //If there are none, then do nothing
-						if(possibleWantedCommands[1] == null) { //If there is only one, then we are good to just use it
-							inputArea.setText(possibleWantedCommands[0] + " "); //Change the input area
-							inputArea.setCaretPosition((possibleWantedCommands[0] + " ").length()); //Put the inputArea cursor at the end
-							break;
-						}
-						//What if there are multiple commands that start with the current string?
-						int commandIndex = inputArea.getText().length()-1;
-						boolean firstCommand = true;
-						boolean done = false;
-						while(true) {
-							firstCommand = true;
-							char character = ' ';
-							for(int p=0; p<i; p++) {
-								String name = possibleWantedCommands[p];
-								if(name.length()-1 < commandIndex) { //Then this is what we will use
-									inputArea.setText(name.substring(0,commandIndex)); //Change the input area
-									inputArea.setCaretPosition(commandIndex); //Put the inputArea cursor at the end
-									done = true;
-									break;
-								}
 
-								if(firstCommand) {
-									character = name.charAt(commandIndex);
-									firstCommand = false;
-								} else if(name.charAt(commandIndex) == character) {
-									continue;
-								} else if(name.charAt(commandIndex) != character) { //there are different ones
-									inputArea.setText(name.substring(0,commandIndex)); //Change the input area
-									inputArea.setCaretPosition(commandIndex); //Put the inputArea cursor at the end
-									done = true;
-									break;
-								}
-							}
-							commandIndex++;
-							if(done) { break; }
-						}
+						//Get the new choice text
+						String newText = autoComplete(inputArea.getText());
+						if(newText.equals(inputArea.getText())) { break; } //Nothing has changed, so dont worry about it
+
+						//Update the gui
+						inputArea.setText(newText); //Change the input area
+						inputArea.setCaretPosition(newText.length()); //Put the inputArea cursor at the end
+						break;
 					default:
 						super.keyReleased(e);
 						break;
@@ -322,11 +285,60 @@ public class GraphicalInterface extends JFrame {
 	}
 
 	/**
+	 * Auto-fills in the possible choice.
+	 * @return          new input area string
+	 * @param inputText The current input area text
+	 */
+	public static String autoComplete(String inputText) {
+		TextFighter.player.getLocation().filterPossibleChoices();
+		String[] possibleWantedCommands = new String[TextFighter.player.getLocation().getPossibleChoices().size()];
+		int i = 0;
+		for(Choice c : TextFighter.player.getLocation().getPossibleChoices()) {
+			if(c.getName().startsWith(inputText)) {
+				possibleWantedCommands[i] = c.getName();
+				i++; //increase in possibleWantedCommands
+			}
+		}
+		int l = possibleWantedCommands.length;
+		System.out.println(l);
+		if(l > 0 && possibleWantedCommands[0] == null) { return inputText; } //If there are none, then do nothing
+		if((l == 1 && possibleWantedCommands[0] != null) || (l > 1 && possibleWantedCommands[1] == null)) { //If there is only one, then we are good to just use it
+			System.out.println("BRUH");
+			return possibleWantedCommands[0] + " ";
+		}
+		//What if there are multiple commands that start with the current string?
+		int commandIndex = inputText.length()-1;
+		boolean firstCommand = true;
+		boolean done = false;
+		while(true) { //This will be broken out of upon `return`
+			firstCommand = true;
+			char character = ' ';
+			for(int p=0; p<i; p++) {
+				String name = possibleWantedCommands[p];
+				if(name.length()-1 < commandIndex) { //No more characters, so this is what we will use
+					return name.substring(0,commandIndex);
+				}
+
+				if(firstCommand) {
+					character = name.charAt(commandIndex);
+					firstCommand = false;
+				} else if(name.charAt(commandIndex) == character) {
+					continue;
+				} else if(name.charAt(commandIndex) != character) { //there are different ones
+					return name.substring(0,commandIndex);
+				}
+			}
+			commandIndex++;
+		}
+	}
+
+
+	/**
 	 * A recursive way to get all the files within a directory as TreeNodes for use by a JTree
 	 * @param parentDirectory   The directory to find more guide files inside
 	 * @return                  The DefaultMutableTreeNode representation of this directory and all its contents and sub-contents
 	 */
-	public DefaultMutableTreeNode getGuideFiles(File parentDirectory) {
+	public static DefaultMutableTreeNode getGuideFiles(File parentDirectory) {
 		//We be using recursion here
 		DefaultMutableTreeNode root = new DefaultMutableTreeNode(parentDirectory.getName());
 		if(parentDirectory.list() == null) { return root; }
